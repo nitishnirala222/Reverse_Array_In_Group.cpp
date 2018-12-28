3955ec7fdc0e71f1c63e88c4250f8aad0f928d95
Nitish Nirala <42496192+nitishnirala222@users.noreply.github.com>
2018-12-28 22:50:01 +0530

    Add files via upload

---
+++ ReverseArrayInGroup.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int reverse(int arr[], int n, int k)
+{
+    for (int i=0;i<n; i+=k)
+    {
+        int left = i;
+        int right = min(i+k-1,n-1);
+        while (left < right)
+            swap(arr[left++], arr[right--]);
+    }
+}
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long int n,k;
+	    cin>>n>>k;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    reverse(arr,n,k);
+	    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+        cout<<endl;
+	}
+	return 0;
+   }
